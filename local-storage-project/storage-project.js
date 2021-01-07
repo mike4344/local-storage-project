7d5de1f0c8a8b15f548ce478066bddf7b1373e73
Mauro Sanchez <novamauro98@gmail.com>
2021-01-07 12:21:54 -0600

    Working on 2nd task.

--- local-storage-project/storage-project.js
+++ local-storage-project/storage-project.js
@@ -1,16 +1,21 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-let form = document.querySelector("form")
-let select = document.getElementById('items')
-let count = 0
+let form = document.querySelector("form");
+let select = document.getElementById('items');
+let shopping = document.getElementById('shopping-cart');
+let quantity = document.getElementById('quantity');
 form.addEventListener('submit',event => {
   event.preventDefault()
-  count ++
-  localStorage.setItem(`cart${count}`, select.value)
-
+  localStorage.setItem(`${select.value}`, quantity.value);
+  showCart()
 })
-  // const showCart = () => {
 
-  // };
+const showCart = () => {
+for (let i = 0; i< localStorage.length; i ++) {
+  console.log(localStorage.key(i))
+}
+};
+
+
 
   // const storeItem = () => {
 

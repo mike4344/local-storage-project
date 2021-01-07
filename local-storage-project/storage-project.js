a1ef0a7b1a2a750d1d5c224ded61cd90ce7ff31b
mike <mikemihalchik@gmail.com>
2021-01-07 13:05:41 -0500

    phase 1

--- local-storage-project/storage-project.js
+++ local-storage-project/storage-project.js
@@ -1,5 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+let form = document.querySelector("form")
+let select = document.getElementById('items')
+let count = 0
+form.addEventListener('submit',event => {
+  event.preventDefault()
+  count ++
+  localStorage.setItem(`cart${count}`, select.value)
 
+})
   // const showCart = () => {
 
   // };
